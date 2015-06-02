4f888f6e599e96ba33497c563847da5e322d6531
asahlin <malakeen@gmail.com>
2015-06-02 17:22:59 +0200

    Merged with new texts, VSCode project

---
+++ gulp/dts.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+
+module.exports = function (name, newName) {
+
+    var file = gulp.src("./bower_components/" + name).pipe(rename(newName));
+    var folder = newName.substring(0, newName.length - 5);
+
+    file.pipe(gulp.dest("./content/scripts/typings/" + folder));
+};
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var gulp = require("gulp");
+var font = require("./gulp/font.js");
+var css = require("./gulp/css.js");
+var js = require("./gulp/js.js");
+var dts = require("./gulp/dts.js");
+
+gulp.task("copy-content", function () {
+
+    font("font-awesome/fonts/*");
+
+    css("bootstrap/dist/css/bootstrap.*");
+    css("font-awesome/css/*.css");
+    //css("toastr/*.css");
+    //css("awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css", "bootstrap_awesome-checkbox.css");
+    //css("awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css", "bootstrap_awesome-checkbox.min.css", true);
+
+    js("bootstrap/dist/js/bootstrap.*");
+    //js("foundation/*.js");
+    js("jquery/dist/*.js");
+//    js("knockout/dist/knockout.debug.js", "knockout.js");
+//    js("knockout/dist/knockout.js", "knockout.min.js");
+//    js("knockout-extensions/*.js");
+//    js("q/q.js");
+//    js("q/q.js", "q.min.js", true);
+//    js("toastr/*.js");
+//    js("fastclick/lib/fastclick.js");
+//    js("fastclick/lib/fastclick.js", "fastclick.min.js", true);
+//
+//    dts("foundation/foundation.d.ts", "foundation.d.ts");
+//    dts("googlemaps.TypeScript.DefinitelyTyped/index.ts", "google-maps.d.ts");
+//    dts("jquery.TypeScript.DefinitelyTyped/index.ts", "jquery.d.ts");
+//    dts("knockout.TypeScript.DefinitelyTyped/index.ts", "knockout.d.ts");
+//    dts("knockout-extensions/knockout-extensions.d.ts", "knockout-extensions.d.ts");
+//    dts("q.TypeScript.DefinitelyTyped/index.ts", "q.d.ts");
+//    dts("toastr.TypeScript.DefinitelyTyped/index.ts", "toastr.d.ts");
+
+});
