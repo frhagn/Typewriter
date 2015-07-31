9c7f69f32f568f67c8ccce16112a041bb869312b
frhagn <fredrik.hagnelius@gmail.com>
2015-07-31 14:37:33 +0200

    added missing file sharedassemblyinfo.cs

---
+++ Typewriter/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("frhagn")]
+[assembly: AssemblyProduct("Typewriter")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: AssemblyVersion("0.9.22.*")]
