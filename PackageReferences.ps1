1bded9ad635d8560c7eb7838fe955fc1026389b9
frhagn <fredrik.hagnelius@gmail.com>
2015-08-08 01:23:26 +0200

    optimized vsix package size

--- PackageReferences.ps1
+++ PackageReferences.ps1
@@ -3,33 +3,29 @@
 #	Set-ExecutionPolicy RemoteSigned
 #
 
-param ([string]$c = "Release")
+param ([string]$configuration = "Release")
 
 Add-Type -Path "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.6\System.IO.Compression.FileSystem.dll"
 
 $invocation = (Get-Variable MyInvocation).Value
 $directorypath = Split-Path $invocation.MyCommand.Path
 
-$zip = [System.IO.Compression.ZipFile]::Open($directorypath + "\Typewriter\bin\" + $c + "\Typewriter.vsix", "Update")
+$zip = [System.IO.Compression.ZipFile]::Open($directorypath + "\Typewriter\bin\" + $configuration + "\Typewriter.vsix", "Update")
 
 # Roslyn dependencies
 "Adding Roslyn dependencies for Visual Studio 2013"
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\Microsoft.CodeAnalysis.CSharp.dll", "Microsoft.CodeAnalysis.CSharp.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\Microsoft.CodeAnalysis.CSharp.Workspaces.dll", "Microsoft.CodeAnalysis.CSharp.Workspaces.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\Microsoft.CodeAnalysis.dll", "Microsoft.CodeAnalysis.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\Microsoft.CodeAnalysis.Workspaces.dll", "Microsoft.CodeAnalysis.Workspaces.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\Microsoft.VisualStudio.ComponentModelHost.dll", "Microsoft.VisualStudio.ComponentModelHost.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\System.Composition.AttributedModel.dll", "System.Composition.AttributedModel.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\System.Composition.Convention.dll", "System.Composition.Convention.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\System.Composition.Hosting.dll", "System.Composition.Hosting.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\System.Composition.Runtime.dll", "System.Composition.Runtime.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\System.Composition.TypedParts.dll", "System.Composition.TypedParts.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\System.Collections.Immutable.dll", "System.Collections.Immutable.dll", "NoCompression")
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $c + "\System.Reflection.Metadata.dll", "System.Reflection.Metadata.dll", "NoCompression")
-
-# Roslyn Metadata
-"Adding Roslyn Metadata Provider"
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Roslyn\bin\" + $c + "\Typewriter.Metadata.Roslyn.dll", "Typewriter.Metadata.Roslyn.dll", "NoCompression")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\Microsoft.CodeAnalysis.CSharp.dll", "Microsoft.CodeAnalysis.CSharp.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\Microsoft.CodeAnalysis.CSharp.Workspaces.dll", "Microsoft.CodeAnalysis.CSharp.Workspaces.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\Microsoft.CodeAnalysis.dll", "Microsoft.CodeAnalysis.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\Microsoft.CodeAnalysis.Workspaces.dll", "Microsoft.CodeAnalysis.Workspaces.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\Microsoft.VisualStudio.ComponentModelHost.dll", "Microsoft.VisualStudio.ComponentModelHost.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\System.Composition.AttributedModel.dll", "System.Composition.AttributedModel.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\System.Composition.Convention.dll", "System.Composition.Convention.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\System.Composition.Hosting.dll", "System.Composition.Hosting.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\System.Composition.Runtime.dll", "System.Composition.Runtime.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\System.Composition.TypedParts.dll", "System.Composition.TypedParts.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\System.Collections.Immutable.dll", "System.Collections.Immutable.dll")
+[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $directorypath + "\Typewriter\bin\" + $configuration + "\System.Reflection.Metadata.dll", "System.Reflection.Metadata.dll")
 
 $zip.Dispose()
 
