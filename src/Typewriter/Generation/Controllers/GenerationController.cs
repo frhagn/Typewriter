60b431f166134dc753fa03bebb6006d65ab215a1
frhagn <fredrik.hagnelius@gmail.com>
2016-01-14 13:29:04 +0100

    added a delay before rendering to wait for roslyn to refresh the current workspace after a change.

--- src/Typewriter/Generation/Controllers/GenerationController.cs
+++ src/Typewriter/Generation/Controllers/GenerationController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using EnvDTE;
 using Typewriter.CodeModel.Implementation;
 using Typewriter.Metadata.Providers;
@@ -23,11 +24,9 @@ namespace Typewriter.Generation.Controllers
             _templateController = templateController;
             _eventQueue = eventQueue;
         }
-
-
+        
         public void OnTemplateChanged(string templatePath)
         {
-            
             Log.Debug("{0} queued {1}", GenerationType.Template, templatePath);
 
             ErrorList.Clear();
@@ -37,50 +36,57 @@ namespace Typewriter.Generation.Controllers
             var template = _templateController.GetTemplate(projectItem);
             var filesToRender = template.GetFilesToRender();
             Log.Debug(" Will Check/Render {0} .cs files in referenced projects", filesToRender.Count);
-            
-            _eventQueue.Enqueue(() =>
+
+            // Delay to wait for Roslyn to refresh the current Workspace after a change.
+            Task.Delay(1000).ContinueWith(task =>
             {
-                var stopwatch = Stopwatch.StartNew();
-                
-
-                foreach (var path in filesToRender)
+                _eventQueue.Enqueue(() =>
                 {
-                    var metadata = _metadataProvider.GetFile(path);
-                    var file = new FileImpl(metadata);
-
-                    template.RenderFile(file);
-
-                    if (template.HasCompileException)
+                    var stopwatch = Stopwatch.StartNew();
+                    
+                    foreach (var path in filesToRender)
                     {
-                        break;
+                        var metadata = _metadataProvider.GetFile(path);
+                        var file = new FileImpl(metadata);
+
+                        template.RenderFile(file);
+
+                        if (template.HasCompileException)
+                        {
+                            break;
+                        }
                     }
-                }
 
-                template.SaveProjectFile();
-
-
-                stopwatch.Stop();
-                Log.Debug("{0} processed {1} in {2}ms", GenerationType.Template, templatePath, stopwatch.ElapsedMilliseconds);
-
+                    template.SaveProjectFile();
+                    
+                    stopwatch.Stop();
+                    Log.Debug("{0} processed {1} in {2}ms", GenerationType.Template, templatePath, stopwatch.ElapsedMilliseconds);
+                });
             });
         }
 
         public void OnCsFileChanged(string[] paths)
         {
-            Enqueue(GenerationType.Render, paths, path => new FileImpl(_metadataProvider.GetFile(path)), (file, template) =>
+            // Delay to wait for Roslyn to refresh the current Workspace after a change.
+            Task.Delay(1000).ContinueWith(task =>
             {
-                if (template.ShouldRenderFile(file.FullName))
+                Enqueue(GenerationType.Render, paths, path => new FileImpl(_metadataProvider.GetFile(path)), (file, template) =>
                 {
-                    template.RenderFile(file);
-                }
+                    if (template.ShouldRenderFile(file.FullName))
+                    {
+                        template.RenderFile(file);
+                    }
+                });
             });
         }
-
-
+        
         public void OnCsFileDeleted(string[] paths)
         {
-            Enqueue(GenerationType.Delete, paths, (path, template) => template.DeleteFile(path));
-
+            // Delay to wait for Roslyn to refresh the current Workspace after a change.
+            Task.Delay(1000).ContinueWith(task =>
+            {
+                Enqueue(GenerationType.Delete, paths, (path, template) => template.DeleteFile(path));
+            });
         }
         private void Enqueue(GenerationType type, string[] paths, Action<string, Template> action)
         {
@@ -89,8 +95,12 @@ namespace Typewriter.Generation.Controllers
 
         public void OnCsFileRenamed(string[] newPaths, string[] oldPaths)
         {
-            Enqueue(GenerationType.Rename, newPaths, (path, fileIndex) => new { OldPath = oldPaths[fileIndex], NewPath = path, NewFile = new FileImpl(_metadataProvider.GetFile(path)) },
+            // Delay to wait for Roslyn to refresh the current Workspace after a change.
+            Task.Delay(1000).ContinueWith(task =>
+            {
+                Enqueue(GenerationType.Rename, newPaths, (path, fileIndex) => new { OldPath = oldPaths[fileIndex], NewPath = path, NewFile = new FileImpl(_metadataProvider.GetFile(path)) },
                 (item, template) => template.RenameFile(item.NewFile, item.OldPath, item.NewPath));
+            });
         }
 
         private void Enqueue<T>(GenerationType type, string[] paths, Func<string, T> transform, Action<T, Template> action)
