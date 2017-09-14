e0fc99320d2b6348e2a60cf9c3c54c0f6621ceb1
mtajmajer <Michael.Tajmajer@Emerson.com>
2017-09-14 08:19:45 -0500

    Added render all menu item

---
+++ src/Typewriter/Generation/SolutionFilesHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Typewriter.Generation.Controllers;
+
+namespace Typewriter.Generation
+{
+    public class SolutionFilesHelper
+    {
+        private DTE dte;
+
+        public SolutionFilesHelper()
+        {
+            dte = (DTE)Package.GetGlobalService(typeof(DTE));
+        }
+
+        public IEnumerable<ProjectItem> Recurse(ProjectItems i)
+        {
+            if (i != null)
+            {
+                foreach (ProjectItem j in i)
+                {
+                    foreach (ProjectItem k in Recurse(j))
+                    {
+                        yield return k;
+                    }
+                }
+
+            }
+        }
+        public IEnumerable<ProjectItem> Recurse(ProjectItem i)
+        {
+            yield return i;
+            foreach (ProjectItem j in Recurse(i.ProjectItems))
+            {
+                yield return j;
+            }
+        }
+
+        public IEnumerable<ProjectItem> SolutionFiles()
+        {
+            foreach (var project in dte.Solution.AllProjects())
+            {
+                foreach (ProjectItem item in Recurse(project.ProjectItems))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
